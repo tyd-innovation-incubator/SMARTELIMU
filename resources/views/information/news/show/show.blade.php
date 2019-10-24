034047e7cacae313d49e9a42578b28b4c79c092a
dontito <francistito07@gmail.com>
2019-10-24 15:58:00 +0300

    working on the new template

--- resources/views/information/news/show/show.blade.php
+++ resources/views/information/news/show/show.blade.php
@@ -31,7 +31,7 @@
                         <div class="blog-posts single-post">
                             <article class="post clearfix mb-0">
                                 <div class="entry-header">
-                                    <div class="post-thumb thumb"> <img src="{!! url('img/news/'.$news->image.'') !!}" alt="" class="img-responsive img-fullwidth"> </div>
+                                    <div class="post-thumb thumb"> <img src="{!! url('http://admin.smartelimu.ac.tz/img/news/'.$news->image.'') !!}" alt="" class="img-responsive img-fullwidth"> </div>
                                 </div>
                                 <div class="entry-content">
                                     <div class="entry-meta media no-bg no-border mt-15 pb-20">
