5d2bcb19853d2b0553f22171b0b67e3799286355
dontito94 <francistito07@gmail.com>
2019-11-12 06:22:19 +0300

    changes on welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -110,55 +110,55 @@
 
 
         <!-- Section: blog -->
-        <section id="blog" class="bg-lighter">
-            <div class="container">
-                <div class="section-title mb-10">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <h2 class="mt-0 text-uppercase font-28 line-bottom line-height-1"> <span class="text-theme-color-2 font-weight-400">{!! trans('label.news_latest') !!}</span></h2>
-                        </div>
-                    </div>
-                </div>
+{{--        <section id="blog" class="bg-lighter">--}}
+{{--            <div class="container">--}}
+{{--                <div class="section-title mb-10">--}}
+{{--                    <div class="row">--}}
+{{--                        <div class="col-md-8">--}}
+{{--                            <h2 class="mt-0 text-uppercase font-28 line-bottom line-height-1"> <span class="text-theme-color-2 font-weight-400">{!! trans('label.news_latest') !!}</span></h2>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-                <div class="section-content">
-                    <div class="row">
-                        @foreach($news as $new)
+{{--                <div class="section-content">--}}
+{{--                    <div class="row">--}}
+{{--                        @foreach($news as $new)--}}
 
-                        <div class="col-xs-12 col-sm-6 col-md-4 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
-                            <article class="post clearfix mb-sm-30">
-                                <div class="entry-header">
-                                    <div class="post-thumb thumb">
-                                        <img src="{!! url('http://admin.smartelimu.ac.tz/img/news/'.$new->image.'') !!}" alt="" class="img-responsive img-fullwidth">
-                                    </div>
-                                </div>
-                                <div class="entry-content p-20 pr-10 bg-white">
-                                    <div class="entry-meta media mt-0 no-bg no-border">
-                                        <div class="entry-date media-left text-center flip bg-theme-colored pt-5 pr-15 pb-5 pl-15">
-                                            <ul>
-                                                <li class="font-16 text-white font-weight-600 border-bottom">{!! $new->created_at->format('d') !!}</li>
-                                                <li class="font-12 text-white text-uppercase">{!! $new->created_at->format('M') !!}</li>
-                                            </ul>
-                                        </div>
-                                        <div class="media-body pl-15">
-                                            <div class="event-content pull-left flip">
-                                                <h4 class="entry-title text-white text-uppercase m-0 mt-5"><a href="#"><b>{!! $new->title !!}</b></a></h4>
-                                                <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-commenting-o mr-5 text-theme-colored"></i> 214 Comments</span>
-                                                <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-heart-o mr-5 text-theme-colored"></i> 895 Likes</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <p class="mt-10">{!! truncateString($new->content,50) !!}</p>
-                                    <a href="{!! route('news.show',$new->slug) !!}" class="btn-read-more">{!! trans('label.read_more') !!}</a>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </article>
-                        </div>
-                            @endforeach
+{{--                        <div class="col-xs-12 col-sm-6 col-md-4 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">--}}
+{{--                            <article class="post clearfix mb-sm-30">--}}
+{{--                                <div class="entry-header">--}}
+{{--                                    <div class="post-thumb thumb">--}}
+{{--                                        <img src="{!! url('http://admin.smartelimu.ac.tz/img/news/'.$new->image.'') !!}" alt="" class="img-responsive img-fullwidth">--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <div class="entry-content p-20 pr-10 bg-white">--}}
+{{--                                    <div class="entry-meta media mt-0 no-bg no-border">--}}
+{{--                                        <div class="entry-date media-left text-center flip bg-theme-colored pt-5 pr-15 pb-5 pl-15">--}}
+{{--                                            <ul>--}}
+{{--                                                <li class="font-16 text-white font-weight-600 border-bottom">{!! $new->created_at->format('d') !!}</li>--}}
+{{--                                                <li class="font-12 text-white text-uppercase">{!! $new->created_at->format('M') !!}</li>--}}
+{{--                                            </ul>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="media-body pl-15">--}}
+{{--                                            <div class="event-content pull-left flip">--}}
+{{--                                                <h4 class="entry-title text-white text-uppercase m-0 mt-5"><a href="#"><b>{!! $new->title !!}</b></a></h4>--}}
+{{--                                                <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-commenting-o mr-5 text-theme-colored"></i> 214 Comments</span>--}}
+{{--                                                <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-heart-o mr-5 text-theme-colored"></i> 895 Likes</span>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <p class="mt-10">{!! truncateString($new->content,50) !!}</p>--}}
+{{--                                    <a href="{!! route('news.show',$new->slug) !!}" class="btn-read-more">{!! trans('label.read_more') !!}</a>--}}
+{{--                                    <div class="clearfix"></div>--}}
+{{--                                </div>--}}
+{{--                            </article>--}}
+{{--                        </div>--}}
+{{--                            @endforeach--}}
 
-                    </div>
-                </div>
-            </div>
-        </section>
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </section>--}}
 
 
 
