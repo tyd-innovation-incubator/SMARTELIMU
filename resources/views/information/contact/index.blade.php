5422e498cc3d06c4356197b91a6f92d2c86c60f8
dontito94 <francistito07@gmail.com>
2019-11-12 23:14:47 +0300

    changes on news interface

--- resources/views/information/contact/index.blade.php
+++ resources/views/information/contact/index.blade.php
@@ -134,7 +134,8 @@
             <div class="container-fluid p-0">
                 <div class="row">
 
-                    <!-- Google Map HTML Codes --><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5456.163483134849!2d144.95177475051227!3d-37.81589041361766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4dd5a05d97%3A0x3e64f855a564844d!2s121+King+St%2C+Melbourne+VIC+3000%2C+Australia!5e0!3m2!1sen!2sbd!4v1556130803137!5m2!1sen!2sbd" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <!-- Google Map HTML Codes -->
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d874.602359373166!2d39.25206318888896!3d-6.829535646622302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4a34a2b6c3af%3A0x586d3256c7115062!2sIlala+Bungoni!5e1!3m2!1sen!2stz!4v1509096721583"  width="100%" height="450" frameborder="0" style="border:0" ></iframe>
                 </div>
             </div>
         </section>
--- resources/views/information/news/search/search.blade.php
+++ resources/views/information/news/search/search.blade.php
@@ -14,71 +14,116 @@
 
 @section('content')
 
+
     <!-- Start main-content -->
+
+
+
     <div class="main-content">
 
-        <!-- Section: inner-header -->
-        {{--<section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="images/bg/bg3.jpg">--}}
-            {{--<div class="container pt-70 pb-20">--}}
-                {{--<!-- Section Content -->--}}
-                {{--<div class="section-content">--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-md-12">--}}
-                            {{--<h2 class="title text-white">Blog</h2>--}}
-                            {{--<ol class="breadcrumb text-left text-black mt-10">--}}
-                                {{--<li><a href="#">Home</a></li>--}}
-                                {{--<li><a href="#">Pages</a></li>--}}
-                                {{--<li class="active text-gray-silver">Page Title</li>--}}
-                            {{--</ol>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</section>--}}
+        <section>
+            <div class="container mt-30 mb-30 pt-30 pb-30">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="blog-posts">
+                            <div class="col-md-12">
+                                <div class="row list-dashed">
+                                    @foreach($all_news as $news)
 
-        <!-- Section: News & Blog -->
-        <section id="news">
-            <div class="container">
-                <div class="section-content">
-                    <div class="row">
-                        @foreach($all_news as $news)
-                        <div class="col-sm-6 col-md-4">
-                            <article class="post clearfix mb-30 mb-sm-30">
-                                <div class="entry-header">
-                                    <div class="post-thumb thumb">
-                                        <img src="{!! url('http://admin.smartelimu.ac.tz/img/news/'.$news->image.'') !!}" alt="" class="img-responsive img-fullwidth">
-                                    </div>
-                                </div>
-                                <div class="entry-content p-20 pr-10 bg-lighter">
-                                    <div class="entry-meta media mt-0 no-bg no-border">
-                                        <div class="entry-date media-left text-center flip bg-theme-colored pt-5 pr-15 pb-5 pl-15">
-                                            <ul>
-                                                <li class="font-16 text-white font-weight-600 border-bottom">{!! $news->created_at->format('d') !!}</li>
-                                                <li class="font-12 text-white text-uppercase">{!! $news->created_at->format('M') !!}</li>
-                                            </ul>
-                                        </div>
-                                        <div class="media-body pl-15">
-                                            <div class="event-content pull-left flip">
-                                                <h4 class="entry-title text-white text-uppercase m-0 mt-5"><a href="{!! route('news.show',$news->slug) !!}">{!! $news->title !!}</a></h4>
-                                                <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-commenting-o mr-5 text-theme-colored"></i> 214 Comments</span>
-                                                <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-heart-o mr-5 text-theme-colored"></i> 895 Likes</span>
+                                    <article class="post clearfix mb-30 pb-30">
+                                        <div class="entry-header">
+                                            <div class="post-thumb thumb">
+                                                <img src="{!! url('http://admin.smartelimu.ac.tz/img/news/'.$news->image.'') !!}" alt="" class="img-responsive img-fullwidth">
                                             </div>
                                         </div>
-                                    </div>
-                                    <p class="mt-10">{!! $news->content !!}</p>
-                                    <a href="{!! route('news.show',$news->slug) !!}" class="btn-read-more">{!! trans('label.read_more') !!}</a>
-                                    <div class="clearfix"></div>
+                                        <div class="entry-content border-1px p-20 pr-10">
+                                            <div class="entry-meta media mt-0 no-bg no-border">
+                                                <div class="entry-date media-left text-center flip bg-theme-colored pt-5 pr-15 pb-5 pl-15">
+                                                    <ul>
+                                                        <li class="font-16 text-white font-weight-600">{!! $news->created_at->format('d') !!}</li>
+                                                        <li class="font-12 text-white text-uppercase">{!! $news->created_at->format('M') !!}</li>
+                                                    </ul>
+                                                </div>
+                                                <div class="media-body pl-15">
+                                                    <div class="event-content pull-left flip">
+                                                        <h4 class="entry-title text-white text-uppercase m-0 mt-5"><a href="{!! route('news.show',$news->slug) !!}">{!! $news->title !!}</a></h4>
+                                                        <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-commenting-o mr-5 text-theme-colored"></i> 214 Comments</span>
+                                                        <span class="mb-10 text-gray-darkgray mr-10 font-13"><i class="fa fa-heart-o mr-5 text-theme-colored"></i> 895 Likes</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <p class="mt-10">{!! truncateString($news->content) !!}</p>
+                                            <a href="{!! route('news.show',$news->slug) !!}" class="btn-read-more">{!! trans('label.read_more') !!}</a>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </article>
+                                        @endforeach
                                 </div>
-                            </article>
+                            </div>
+                            <div class="col-md-12">
+                                <nav>
+                                    <ul class="pagination theme-colored">
+                                        <li> <a aria-label="Previous" href="#"> <span aria-hidden="true">«</span> </a> </li>
+                                        <li class="active"><a href="#">1</a></li>
+                                        <li><a href="#">2</a></li>
+                                        <li><a href="#">3</a></li>
+                                        <li><a href="#">4</a></li>
+                                        <li><a href="#">5</a></li>
+                                        <li><a href="#">...</a></li>
+                                        <li> <a aria-label="Next" href="#"> <span aria-hidden="true">»</span> </a> </li>
+                                    </ul>
+                                </nav>
+                            </div>
                         </div>
-                            @endforeach
+                    </div>
+                    <div class="col-md-3">
+                        <div class="sidebar sidebar-right mt-sm-30">
+                            <div class="widget">
+                                <h5 class="widget-title line-bottom">Search box</h5>
+                                <div class="search-form">
+                                    <form>
+                                        <div class="input-group">
+                                            <input type="text" placeholder="Click to Search" class="form-control search-input">
+                                            <span class="input-group-btn">
+                      <button type="submit" class="btn search-button"><i class="fa fa-search"></i></button>
+                      </span>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <h5 class="widget-title line-bottom">Categories</h5>
+                                <div class="categories">
+                                    <ul class="list list-border angle-double-right">
+                                        <li><a href="#">Ubunifu<span>(0)</span></a></li>
+                                        <li><a href="#">Tehama<span>(0)</span></a></li>
+                                        <li><a href="#">Ugunduzi<span>(0)</span></a></li>
+                                        <li><a href="#">Maonyesho<span>(0)</span></a></li>
+                                        <li><a href="#">Michezo<span>(0)</span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <h5 class="widget-title line-bottom">Latest News</h5>
+                                <div class="latest-posts">
+                                    @foreach($all_news as $news)
+                                    <article class="post media-post clearfix pb-0 mb-10">
+                                        <a class="post-thumb" href="#"><img src="{!! url('http://admin.smartelimu.ac.tz/img/news/'.$news->image.'') !!}" alt=""></a>
+                                        <div class="post-right">
+                                            <h5 class="post-title mt-0"><a href="#">{!! $news->title !!}</a></h5>
+                                            <p>{!! truncateString($news->content,50) !!}</p>
+                                        </div>
+                                    </article>
+                                        @endforeach
 
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
     </div>
-
 
 
 
